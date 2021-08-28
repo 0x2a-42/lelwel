bd77d4a1d0c37efe5a93241b876a34dbda8a6379
Daniel Pfister <80419299+0x2a-42@users.noreply.github.com>
2021-08-28 23:07:25 +0200

    Fix bug for action and predicate definitions
    There was no error message for action and predicate definitions that
    referenced undefined rules.

--- src/frontend/sema.rs
+++ src/frontend/sema.rs
@@ -212,6 +212,12 @@ impl<'a, 'b> GeneralValidator {
                     diag.error(Code::LowercaseToken(*name), element.range());
                 }
             }
+            ElementKind::Action { name, .. }
+            | ElementKind::Predicate { name, .. } => {
+                if !bindings.contains_key(&Binding::Term(*name)) {
+                    diag.error(Code::UndefinedElement(*name), element.range());
+                }
+            }
             _ => {}
         }
     }
