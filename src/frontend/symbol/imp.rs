3271078b51eb97a3a81c143b78e36ed9892aa920
Daniel Pfister <80419299+0x2a-42@users.noreply.github.com>
2022-06-15 19:02:55 +0200

    Move macro `predefine` to other file

--- src/frontend/symbol/imp.rs
+++ src/frontend/symbol/imp.rs
@@ -1,28 +1,5 @@
 use super::*;
 
-macro_rules! predefine {
-    ( $([$id:ident, $name:expr]),* $(,)? ) => {
-        #[repr(usize)]
-        #[allow(clippy::upper_case_acronyms)]
-        enum Predef {
-            EMPTY,
-            $($id),*
-        }
-        impl Symbol {
-            pub const EMPTY: Symbol = Symbol(Predef::EMPTY as u32);
-            $(
-            pub const $id: Symbol = Symbol(Predef::$id as u32);
-            )*
-        }
-        impl StringTable {
-            pub fn init(&mut self) {
-                self.alloc("");
-                $(self.alloc($name);)*
-            }
-        }
-    }
-}
-
 predefine! {
     [START,      "start"],
     [EOF,        "EOF"],
--- src/frontend/symbol/mod.rs
+++ src/frontend/symbol/mod.rs
@@ -169,3 +169,27 @@ impl StringTable {
         self.table[num as usize]
     }
 }
+
+macro_rules! predefine {
+    ( $([$id:ident, $name:expr]),* $(,)? ) => {
+        #[repr(usize)]
+        #[allow(clippy::upper_case_acronyms)]
+        enum Predef {
+            EMPTY,
+            $($id),*
+        }
+        impl Symbol {
+            pub const EMPTY: Symbol = Symbol(Predef::EMPTY as u32);
+            $(
+            pub const $id: Symbol = Symbol(Predef::$id as u32);
+            )*
+        }
+        impl StringTable {
+            pub fn init(&mut self) {
+                self.alloc("");
+                $(self.alloc($name);)*
+            }
+        }
+    }
+}
+pub(crate) use predefine;
