52d417f4a1474aef7298bf2b78fdc9bfda23382d
Daniel Pfister <80419299+0x2a-42@users.noreply.github.com>
2025-04-19 12:47:38 +0200

    Add TOML example (#21)

---
+++ examples/toml/tests/test.rs
@@ -0,0 +1,21 @@
+use lelwel_toml::generate_syntax_tree;
+
+macro_rules! check {
+    ($file:literal) => {
+        let res =
+            generate_syntax_tree(&include_str!(concat!("data/", $file, ".toml")).replace('\r', ""));
+        assert_eq!(
+            format!("{}", res[0]),
+            include_str!(concat!("data/", $file, ".tree")).replace('\r', "")
+        );
+        assert_eq!(
+            format!("{}", res[1]),
+            include_str!(concat!("data/", $file, ".diag")).replace('\r', "")
+        );
+    };
+}
+
+#[test]
+fn incomplete() {
+    check!("incomplete");
+}
