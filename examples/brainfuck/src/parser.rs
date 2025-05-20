3f5b163f00fc3b9d98142ef2086ea73b94aedeb9
Daniel Pfister <80419299+0x2a-42@users.noreply.github.com>
2025-05-20 21:49:34 +0200

    Remove TODO comments from example and run cargo fmt

--- examples/brainfuck/src/parser.rs
+++ examples/brainfuck/src/parser.rs
@@ -1,13 +1,11 @@
-use super::lexer::{Token, tokenize};
+use super::lexer::{tokenize, Token};
 use codespan_reporting::diagnostic::Label;
 
-// TODO: change definition and all uses if codespan_reporting is not used
 pub type Diagnostic = codespan_reporting::diagnostic::Diagnostic<()>;
 
-// TODO: add context information to the parser if required
 #[derive(Default)]
 pub struct Context<'a> {
-    marker: std::marker::PhantomData<&'a ()>
+    marker: std::marker::PhantomData<&'a ()>,
 }
 
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
