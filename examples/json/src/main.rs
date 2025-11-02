27b4becfe5accb3ad77cbfb2004041bda1c6abd5
Daniel Pfister <80419299+0x2a-42@users.noreply.github.com>
2025-11-02 21:52:11 +0100

    Create owned value in json example (#63)

--- examples/json/src/main.rs
+++ examples/json/src/main.rs
@@ -5,7 +5,70 @@ use codespan_reporting::diagnostic::Severity;
 use codespan_reporting::files::SimpleFile;
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 use codespan_reporting::term::{self, Config};
+use lexer::Token;
 use parser::*;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum Value {
+    Null,
+    Bool(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Value>),
+    Object(HashMap<String, Value>),
+}
+
+impl Cst<'_> {
+    pub fn to_value(&self, node_ref: NodeRef) -> Option<Value> {
+        match self.get(node_ref) {
+            Node::Rule(rule, _) => match rule {
+                Rule::File | Rule::Literal => self.to_value(self.children(node_ref).next()?),
+                Rule::Array => Some(Value::Array(
+                    self.children(node_ref)
+                        .filter_map(|child_node_ref| self.to_value(child_node_ref))
+                        .collect(),
+                )),
+                Rule::Object => {
+                    let mut members = HashMap::new();
+                    for mut member_node_refs in self
+                        .children(node_ref)
+                        .filter(|&child_node_ref| self.match_rule(child_node_ref, Rule::Member))
+                        .map(|child_node_ref| self.children(child_node_ref))
+                    {
+                        let Some(key) = member_node_refs
+                            .find_map(|member_node_ref| {
+                                self.match_token(member_node_ref, Token::String)
+                            })
+                            .map(|(key_str, _)| key_str[1..key_str.len() - 1].to_owned())
+                        else {
+                            continue;
+                        };
+                        let Some(val) = member_node_refs
+                            .find_map(|member_node_ref| self.to_value(member_node_ref))
+                        else {
+                            continue;
+                        };
+                        members.insert(key, val);
+                    }
+                    Some(Value::Object(members))
+                }
+                _ => None,
+            },
+            Node::Token(token, idx) => match token {
+                Token::String => {
+                    let val = self.span_text(idx);
+                    Some(Value::String(val[1..val.len() - 1].to_owned()))
+                }
+                Token::Number => Some(Value::Number(str::parse(self.span_text(idx)).ok()?)),
+                Token::True => Some(Value::Bool(true)),
+                Token::False => Some(Value::Bool(false)),
+                Token::Null => Some(Value::Null),
+                _ => None,
+            },
+        }
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
@@ -16,7 +79,9 @@ fn main() -> std::io::Result<()> {
     let source = std::fs::read_to_string(&args[1])?;
     let mut diags = vec![];
     let cst = Parser::new(&source, &mut diags).parse(&mut diags);
+    let val = cst.to_value(NodeRef::ROOT);
     println!("{cst}");
+    println!("{val:#?}");
 
     let writer = StandardStream::stderr(ColorChoice::Auto);
     let config = Config::default();
